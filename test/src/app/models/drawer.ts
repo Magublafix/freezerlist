02ddbffb6bba72760d8bd90d53e04f128ad9cdd2
El-Sueco <svenstauder@outlook.at>
2024-03-24 13:44:51 +0100

    test if blank copied material project works

---
+++ test/src/app/models/drawer.ts
@@ -0,0 +1,4 @@
+export interface Drawer {
+    id?: number;
+    physicalOrder: number;
+}
---
+++ test/src/app/models/freezeritem.ts
@@ -0,0 +1,6 @@
+export interface FreezerItem {
+    id?: number | null;
+    content: string | null;
+    drawerId: number | null;
+    freezedate: string | null;
+}
